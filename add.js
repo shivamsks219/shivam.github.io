b0cd9322a8fe2a107dc08b195321a6136ec60cfc
Shivam Kumar <68140446+shivamsks219@users.noreply.github.com>
2023-05-29 17:13:17 +0530

    add typed effect

---
+++ add.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text",{
+    strings: ["Shivam Kumar", "a Software Developer", "a Creative Person"],
+    typeSpeed: 100,
+    backSpedd: 100,
+    backDelay:1000,
+    loop:true
+})
